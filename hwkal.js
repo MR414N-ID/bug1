98e5988ae6bd4a6f55c8e87dbec46fc8b1c506cb
MR414N-ID <mr.414nn@gmail.com>
2023-08-14 17:31:42 -0400

    bug1

---
+++ hwkal.js
@@ -0,0 +1,50 @@
+// Buat Lu Yang Jual Sc Ini Yang Jujur Jangan Samp Nipu Apalagi Lari Dari Tanggung Jawab
+
+// Base Haikal Hw Mods : Copyan Dari Base Dika
+
+
+const fs = require('fs')
+const chalk = require('chalk')
+const tiktok = require("./baseikal/download/tiktok")
+const facebook = require("./baseikal/download/facebook")
+const instagram = require("./baseikal/download/instagram")
+const twitter = require("./baseikal/download/twitter")
+
+
+//MODULNYA BY HW
+global.baileys = require('@whiskeysockets/baileys') 
+global.adiwajshing = require('@adiwajshing/baileys') 
+
+global.gr = 'https://chat.whatsapp.com/IfWgGV9jV2NBEWXYZCK4NN'
+global.ig = '@faallzst_' // ubah aja
+global.email = 'faallzofficial@gmail.com' //serah
+global.region = 'indonesia' // serah
+//—————「 Set Nama Own & Bot 」—————//
+global.ownername = 'deril offc' //ubah jadi nama mu, note tanda ' gausah di hapus!
+//=================================================//
+global.owner = ['6281362459904'] // ubah aja pake nomor lu
+//==========================BY Hw Mods=======================//
+global.keyopenai = `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`
+//====================BY Hw Mods=============================//
+global.botname = 'FallBot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.packname = 'Bot Whatsapp' // ubah aja ini nama sticker
+global.author = '© FaallzBotz' // ubah aja ini nama sticker
+global.prefa = ['','!','.',',','🐤','🗿']
+global.sessionName = 'haikal' //Gausah Juga
+global.sp = '⭔' // Gausah Juga
+global.wlcm = []
+global.wlcmm = []
+//=================================================//
+//Gausah Juga
+global.limitawal = {
+    premium: "Infinity",
+    free: 10
+}
+//=================================================//
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
